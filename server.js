ff7ed86e39e0b49b8d5782e81ad27425cedf2e33
RanganathG <ranganath.govardhanagiri@gmail.com>
2017-08-28 19:20:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express')
 var app = express()
+var port = process.env.PORT || 1337;
 
 app.get('/', function (req, res) {
   res.send('Hello World again!')
 })
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!')
 })
